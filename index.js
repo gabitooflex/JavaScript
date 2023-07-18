df540c57293c7cc6001e5318e00c2b56aa9248c8
Gabriel Rodriguez <gabo.rodriguez.169@gmail.com>
2023-07-17 22:27:22 -0300

    Correcion en el if edadUsuario

--- index.js
+++ index.js
@@ -18,30 +18,31 @@ while (edadUsuario == "" || edadUsuario <= 0 ) {
     edadUsuario = prompt("Ingrese su año de nacimiento")
 }
 
-if (edadUsuario > 2005 ) {
-    alert("Usted es menor de edad");
-} else if ( edadUsuario <= 2005) {
+if ( edadUsuario <= 2005) {
     alert("Usted es mayor de edad");
-}
+        let cantidadDeEntradas = prompt("Indique el numero de entradas que quiere (3 como máximo)")
 
-let cantidadDeEntradas = prompt("Indique el numero de entradas que quiere (3 como máximo)")
-
-while (cantidadDeEntradas >3 || cantidadDeEntradas == "" || cantidadDeEntradas == 0 ){
-    alert("Usted no ha ingresado un valor correcto")
-    cantidadDeEntradas = prompt("Indique el numero de entradas que quiere (3 como máximo)")
-}
-
-
-for(let i = 1; i <= cantidadDeEntradas; i++) {
-    let ingresarNombre = prompt("Nombre para su entrada gratuita");
-    while (ingresarNombre == "" ){
-        alert("No ha ingresado un nombre para su entrada")
-        ingresarNombre = prompt("Nombre para su entrada gratuita");
+    while (cantidadDeEntradas >3 || cantidadDeEntradas == "" || cantidadDeEntradas == 0 ){
+        alert("Usted no ha ingresado un valor correcto")
+        cantidadDeEntradas = prompt("Indique el numero de entradas que quiere (3 como máximo)")
     }
-    alert("Entrada N° " + i + " a nombre de " + ingresarNombre)
+
+
+    for(let i = 1; i <= cantidadDeEntradas; i++) {
+        let ingresarNombre = prompt("Nombre para su entrada gratuita");
+        while (ingresarNombre == "" ){
+            alert("No ha ingresado un nombre para su entrada")
+            ingresarNombre = prompt("Nombre para su entrada gratuita");
+        }
+        alert("Entrada N° " + i + " a nombre de " + ingresarNombre)
+    }
+
+
+    alert("Usted adquirió " + cantidadDeEntradas  + " entrada, lo esperamos el 25/08 en nuestro boliche" )
 }
+else if (edadUsuario > 2005 ) {
+    alert("Usted es menor de edad");
+} 
 
 
-alert("Usted adquirió " + cantidadDeEntradas  + " entrada, lo esperamos el 25/08" )
-
-alert("Muchas gracias por su visita " + nombreUsuario + " Lo esperamos en FLX!");
+alert("Muchas gracias por su visita " + nombreUsuario + "!");
